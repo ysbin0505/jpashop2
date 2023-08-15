687665addf560aa31acd4f20e4b481e15d78b6ce
ysbin0505@naver.com <118337787+ysbin0505@users.noreply.github.com>
2023-08-15 20:12:13 +0900

    jpa활용 시작- DB 동작 확인

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -9,8 +9,6 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.Assert.*;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MemberRepositoryTest {
