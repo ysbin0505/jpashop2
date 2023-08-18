76d46388c85c801d71ba45b4151dc9e3438aee15
ysbin0505@naver.com <118337787+ysbin0505@users.noreply.github.com>
2023-08-18 15:00:22 +0900

    엔티티 클래스 개발 H2까지 연동생성 완료

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -5,9 +5,18 @@ import lombok.Getter;
 import javax.persistence.Embeddable;
 
 @Embeddable
-@Getter
+@Getter   //값 타입은 변경 불가능하게 설계 -> @Setter 제거, 생성자에서 값을 모두 초기화해서 변경 불가능한 클래스 만들기.
 public class Address {
   private String city;
   private String street;
   private String zipcode;
+
+  protected Address(){
+  }
+
+  public Address(String city, String street, String zipcode) {
+    this.city = city;
+    this.street = street;
+    this.zipcode = zipcode;
+  }
 }
