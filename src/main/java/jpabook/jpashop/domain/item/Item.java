c51e04bf40e0c029c87fd426460931ccbb4109f0
ysbin0505@naver.com <118337787+ysbin0505@users.noreply.github.com>
2023-08-20 19:42:37 +0900

    상품 서비스 개발

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -23,5 +24,18 @@ public abstract class Item {
   @ManyToMany(mappedBy = "items")
   private List<Category> categories = new ArrayList<>();
 
+  //=비즈니스 로직=//
+  
+  public void addStock(int quantity){
+    this.stockQuantity += quantity;   //stock 증가
+  }
+
+  public void removeStock(int quantity){
+    int restStock = this.stockQuantity - quantity;    //stock 감소
+    if (restStock < 0 ){
+      throw new NotEnoughStockException("need more stock");
+    }
+    this.stockQuantity = restStock;
+  }
 
 }
