4c2441768d24123518b25de41fec6a3c845da294
ysbin0505@naver.com <118337787+ysbin0505@users.noreply.github.com>
2023-08-20 14:16:45 +0900

    회원가입 기능, 회원 조회기능 주가

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional(readOnly = true)
 public class MemberService {
@@ -18,10 +20,27 @@ public class MemberService {
    */
   @Transactional //변경
   public Long join(Member member){
-    
+    validateDuplicateMember(member);    //중복 회원 검증
+    memberRepository.save(member);
+    return member.getId();   //em.persist 아직 db에 들어간 시점이 아니라도 값을 넣어줌
+  }
+
+  private void validateDuplicateMember(Member member) {
+    //EXCEPTION
+    List<Member> findMembers = memberRepository.findByName(member.getName());
+    if (!findMembers.isEmpty()){
+      throw new IllegalStateException("이미 존재하는 회원입니다.");   //member수를 카운트해서 0보다 크다 라고 하면 더 최적화 되긴함
+    }
   }
   
   /*
-  @@회원 전체 조회
+  @@회원 조회
    */
+  public List<Member> findMembers(){
+    return memberRepository.findAll(); //전체 조회
+  }
+
+  public Member findOne(Long memberId){
+    return memberRepository.findOne(memberId);
+  }
 }
