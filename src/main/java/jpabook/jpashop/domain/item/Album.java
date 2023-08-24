acbe3d52018f58e464c9aff0e5637c0853f6d12a
ysbin0505@naver.com <118337787+ysbin0505@users.noreply.github.com>
2023-08-24 14:10:23 +0900

    웹 계층 개발 2. 회원 목록 조회

--- src/main/java/jpabook/jpashop/domain/item/Album.java
+++ src/main/java/jpabook/jpashop/domain/item/Album.java
@@ -9,7 +9,7 @@ import javax.persistence.Entity;
 @Entity
 @DiscriminatorValue("A")
 @Getter @Setter
-public class Album extends Item{
+public class Album extends Item {
   private String artist;
   private String etc;
 }
---
+++ src/main/java/jpabook/jpashop/web/AlbumForm.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop.web;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+
+public class AlbumForm {
+
+  private Long id;
+
+  private String name;
+  private int price;
+  private int stockQuantity;
+
+  private String artist;
+  private String etc;
+}
