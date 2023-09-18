d6d13166d69f818e2bbb067b1aaefbae7370e0df
ysbin0505 <chfhddl515@gmail.com>
2023-09-18 11:51:08 +0900

    jpa2 시작

---
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,10 @@
+package jpabook.jpashop.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller @ResponseBody
+@RestController
+public class MemberApiController {
+}
